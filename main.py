d3d95e023d244b6647ba079b8379e9ca536513a6
Zachary Lee <zlee314@gmail.com>
2022-03-12 23:20:16 -0700

    clean up

--- main.py
+++ main.py
@@ -5,46 +5,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import time
+from sys import argv
 import tkinter
 
 
+def main(filename):
 
+    image = Image.open(filename)
 
-image = Image.open("actual_grey.png")
-#image.show()
+    A = asarray(image)
 
-A = asarray(image)
+    u, s, vh = svd(A)
 
-u, s, vh = svd(A)
-
-print(f'A: {A.shape}, u: {u.shape}, vh: {vh.shape}')
-
-def ith_singular_product(i: int):
     win = tkinter.Tk()
     win.geometry("750x270")
-    #Create a canvas
     canvas= tkinter.Canvas(win, width= 600, height= 400)
     canvas.pack()
     text_str = tkinter.StringVar()
     text = tkinter.Label(win, textvariable=text_str)
     text.pack()
-    #Load an image in the script
     sum = np.zeros(A.shape)
-    for count in range(i):
-        sum += s[count]*(u[:,[count]] @ vh[[count],])
+    for i in range(len(s)):
+        sum += s[i]*(u[:,[i]] @ vh[[i],])
         pil_image = Image.fromarray(sum)
         img = ImageTk.PhotoImage(pil_image)
         canvas.create_image(10,10,anchor=tkinter.NW,image=img)
-        percent = count * (u.shape[0] + vh.shape[0]) / (A.shape[0] * A.shape[1])*100
-        text_str.set(f'Singular Values: {count}\n{percent:.2f}% of original size')
+        percent = i * (u.shape[0] + vh.shape[0]) / (A.shape[0] * A.shape[1])*100
+        text_str.set(f'Singular Values: {i} of {len(s)}\n{percent:.2f}% of original size\nCurrent SV: {s[i]:.3e}\nMax SV: {s[0]:e}')
         win.update()
         time.sleep(0.2)
 
     win.mainloop() 
 
 
-    return sum
-
-first_singular = ith_singular_product(len(s))
-
-compressed = Image.fromarray(first_singular)
+if __name__ == '__main__':
+    main(argv[1])
